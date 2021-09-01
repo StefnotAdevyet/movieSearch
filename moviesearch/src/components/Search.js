695855c09480ab1a7676850d8b577f6a00bc58f2
stefnotadevyet <srjack91@gmail.com>
2021-08-31 22:13:46 -0400

    added input form to Search

--- moviesearch/src/components/Search.js
+++ moviesearch/src/components/Search.js
@@ -5,6 +5,11 @@ const Search = () => {
     <div className="container">
       <div className="row">
         <section className="col s4 offset-4">
+          <form action="">
+            <div className="input-field">
+              <input placeholder="Search Movie" type="text" />
+            </div>
+          </form>
 
         </section>
       </div>
