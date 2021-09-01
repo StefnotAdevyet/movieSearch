80f28a1c17da8fe24ffb69e2fbc9185764e07483
stefnotadevyet <srjack91@gmail.com>
2021-08-31 21:48:08 -0400

    added search function component

--- moviesearch/src/components/App.js
+++ moviesearch/src/components/App.js
@@ -1,12 +1,14 @@
 import '../App.css';
 import React from 'react'
 import Nav from './Nav'
+import Search from './Search';
 
 class App extends React.Component {
   render() {
   return (
     <div className="App">
       <Nav />
+      <Search />
 
     </div>
   );
---
+++ moviesearch/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <section className="col s4 offset-4">
+
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Search;
