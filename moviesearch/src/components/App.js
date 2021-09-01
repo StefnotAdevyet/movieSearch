815a84fbc6d528379b1a5b50ae68915b5313d9c8
stefnotadevyet <srjack91@gmail.com>
2021-09-01 19:02:06 -0400

    began writing event handlers

--- moviesearch/src/components/App.js
+++ moviesearch/src/components/App.js
@@ -1,14 +1,33 @@
 import '../App.css';
-import React from 'react'
-import Nav from './Nav'
+import React from 'react';
+import Nav from './Nav';
 import Search from './Search';
+import axios from 'axios';
+
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      searchTerm: ''
+    }
+    this.apiKey = process.env.APP_API
+  }
+
+  handleSubmit = (e) => {
+
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
   render() {
   return (
     <div className="App">
       <Nav />
-      <Search />
+      <Search handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
 
     </div>
   );
