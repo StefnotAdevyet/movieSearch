1f08c8ba15d8ed3dfd92a2c20ae13c1f4f4c727b
stefnotadevyet <srjack91@gmail.com>
2021-08-31 21:19:46 -0400

    added Nav function component

---
+++ moviesearch/src/components/App.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import React from 'react'
+import Nav from './Nav'
+
+class App extends React.Component {
+  render() {
+  return (
+    <div className="App">
+      <Nav />
+
+    </div>
+  );
+  }
+}
+
+export default App;
